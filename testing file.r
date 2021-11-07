b6586957603e409356232103d48cb40c797387b0
Mudassir Mustaqeem <marketingchannel786@gmail.com>
2021-11-07 18:13:08 +0100

    Few lines were added

--- testing file.r
+++ testing file.r
@@ -1,2 +1,9 @@
-This is from Git
+#This is from Git
 print('Hello from Github')
+
+name = 'Mudassir'
+Version = 'RStudio - In writing'
+
+print(name)
+print('These lines have been written in our Studio')
+
